d09f51db5f3a0e4ee7e8ef3f86fe943b81b2b3ee
DuocCosas <jor.videla@profesor.duoc.cl>
2024-10-01 23:14:45 -0300

    a

--- app_proyecto/capacitor.config.ts
+++ app_proyecto/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'cl.duoc.miproyecto',
   appName: 'app_proyecto',
   webDir: 'www'
 };
