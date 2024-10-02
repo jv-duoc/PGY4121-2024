8f3b4b1ccb32f2b48ec385fdcf747e8e034dc503
Jorge Videla <jorge.videla.v@gmail.com>
2024-10-02 00:23:44 -0300

    promesa simplificada

--- app_proyecto/src/app/services/login.service.ts
+++ app_proyecto/src/app/services/login.service.ts
@@ -8,22 +8,16 @@ import { BehaviorSubject, lastValueFrom } from 'rxjs';
 export class LoginService {
 
   user:User|null = null;
-
-  //creamos un metodo que llamares cuando firebase efectivamente se haya cargado
-  cargarFirebase!: () => void;
-
   //creamos una promesa que se resuelve cuando firebase se haya cargado
-  firebaseCargado = new Promise<void>((resolve)=>{
-    this.cargarFirebase = resolve;
-  });
+  firebaseCargado: Promise<void>;
 
   constructor() {
-    FirebaseAuthentication.getCurrentUser();
-    FirebaseAuthentication.addListener('authStateChange',(status)=>{
-      this.user = status.user;
-      this.cargarFirebase();
-    })
-    
+    this.firebaseCargado = new Promise<void>((resolve)=>{
+      FirebaseAuthentication.addListener('authStateChange',(status)=>{
+        this.user = status.user;
+        resolve();// con esto le decimos que ya cargo firebase, resolvemos la promesa
+      })
+    });
   }
 
 
